ada7dccf8581a3938f69803fa50c8db8e56323a6
Matt Cottingham <matt.cottingham@gmail.com>
2013-05-03 21:16:48 +0100

    Parameterise build_url to use API scheme, version

--- microweb/helpers.py
+++ microweb/helpers.py
@@ -2,22 +2,25 @@ import datetime
 import json
 import re
 
+from settings import API_VERSION
+from settings import API_PATH
 from settings import API_SCHEME
 from settings import API_DOMAIN_NAME
+from settings import DEBUG
 
 # A hack used when converting timestamp strings to datetime.datetime instances
 VALID_DATETIME = re.compile(r'^\d{4}-\d{2}-\d{2}[ T]\d{2}:\d{2}:\d{2}.?\d{0,6}Z$')
 
-def build_url(host, path_fragments=None, api_scheme=API_SCHEME):
+def build_url(host, path_fragments):
     """
     urljoin and os.path.join don't behave exactly as we want, so
     here's a different wheel.
 
     As per RFC 3986, authority is composed of hostname[:port] (and optionally
     userinfo, but the microcosm API will never accept these in the URL, so
-    we ignore them).
+    we ignore their presence).
 
-    path should be a list of fragments. This function will strip slashes and
+    path should be a list of URL fragments. This function will strip separators and
     insert them where needed to form a valid URL.
 
     The use of + for string concat is deemed acceptable because it is 'fast enough'
@@ -27,16 +30,19 @@ def build_url(host, path_fragments=None, api_scheme=API_SCHEME):
     if not host.endswith(API_DOMAIN_NAME):
         raise AssertionError('Custom domains are not yet supported on Microcosm')
 
-    authority = api_scheme + host
+    url = API_SCHEME + host
+    path_fragments = [API_PATH, API_VERSION] + path_fragments
 
     for fragment in path_fragments:
+        if not isinstance(fragment, str):
+            fragment = str(fragment)
         if '/' in fragment:
             fragment = fragment.strip('/')
             if '/' in fragment:
                 raise AssertionError('Do not use path fragments containing slashes')
-        authority += ('/' + fragment)
+        url += ('/' + fragment)
 
-    return authority
+    return url
 
 
 class DateTimeEncoder(json.JSONEncoder):
