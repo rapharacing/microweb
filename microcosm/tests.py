0a304b276b14f69592996d7deff2ab45a75650ec
Matt Cottingham <matt.cottingham@gmail.com>
2013-05-07 10:28:48 +0100

    Test that request host is used to call construct API url
    For all resources, the request host must be used to call the backend.
    
    This test patches requests.get and verifies the correct API url is being called.

--- microcosm/tests.py
+++ microcosm/tests.py
@@ -1,5 +1,16 @@
+import random
+import string
+from mock import patch
+
 from django.utils import unittest
+from django.test.client import RequestFactory
+
+from microcosm.views import MicrocosmView
+
 from microweb.helpers import build_url
+from microweb.settings import API_SCHEME
+from microweb.settings import API_DOMAIN_NAME
+
 
 class BuildURLTests(unittest.TestCase):
     """
@@ -37,3 +48,34 @@ class BuildURLTests(unittest.TestCase):
     def testFailCustomDomains(self):
         with self.assertRaises(AssertionError):
             build_url('a.example.org', ['resource', '1', 'ex/tra'])
+
+
+class RoutingAPITests(unittest.TestCase):
+    """
+    Verify that the Host header is used to call the appropriate
+    API endpoint.
+    """
+
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def testMicrocosmsView(self):
+
+        # Construct a random subdomain string
+        subdomain = ''
+        for x in xrange(10):
+            subdomain += random.choice(string.lowercase)
+        host = '%s.microco.sm' % subdomain
+
+        # Create a request for a list of microcosms
+        request = self.factory.get('/microcosms', HTTP_HOST=host)
+        request.access_token = None
+        request.whoami = None
+        request.site = None
+
+        # Patch requests.get and check the call args
+        with patch('requests.get') as mock:
+            mock.return_value.json.return_value = {'error': None, 'data': {'hi': 5}}
+            MicrocosmView.list(request)
+            path = build_url(host, ['microcosms'])
+            mock.assert_called_once_with(path, headers={'Host': host}, params={})
