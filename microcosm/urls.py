6ccc159eecc0f5c3bfac534103ee18265c486d28
Matt Cottingham <matt.cottingham@gmail.com>
2013-07-01 13:32:12 +0100

    Supply event_id to EventView methods

--- microcosm/urls.py
+++ microcosm/urls.py
@@ -33,9 +33,9 @@ urlpatterns = patterns('',
 
     # Events
     url(r'^microcosms/(?P<microcosm_id>\d+)/create/event/$', EventView.create, name='create-event'),
-    url(r'^events/(?P<item_id>\d+)/$', EventView.single, name='single-event'),
-    url(r'^events/(?P<item_id>\d+)/edit/$', EventView.edit, name='edit-event'),
-    url(r'^events/(?P<item_id>\d+)/delete/$', EventView.delete, name='delete-event'),
+    url(r'^events/(?P<event_id>\d+)/$', EventView.single, name='single-event'),
+    url(r'^events/(?P<event_id>\d+)/edit/$', EventView.edit, name='edit-event'),
+    url(r'^events/(?P<event_id>\d+)/delete/$', EventView.delete, name='delete-event'),
     # RSVP to an event
     url(r'^events/(?P<event_id>\d+)/rsvp/$', EventView.rsvp, name='rsvp-event'),
 
