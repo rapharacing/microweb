1275e14aa95c05b000d709a6af8986fb74e0205c
Matt Cottingham <matt.cottingham@gmail.com>
2013-07-01 22:12:55 +0100

    Route conversation views with conversation_id

--- microcosm/urls.py
+++ microcosm/urls.py
@@ -41,9 +41,9 @@ urlpatterns = patterns('',
 
     # Conversations
     url(r'^microcosms/(?P<microcosm_id>\d+)/create/conversation/$', ConversationView.create, name='create-conversation'),
-    url(r'^conversations/(?P<item_id>\d+)/$' , ConversationView.single, name='single-conversation'),
-    url(r'^conversations/(?P<item_id>\d+)/edit/$', ConversationView.edit, name='edit-conversation'),
-    url(r'^conversations/(?P<item_id>\d+)/delete/$', ConversationView.delete, name='delete-conversation'),
+    url(r'^conversations/(?P<conversation_id>\d+)/$' , ConversationView.single, name='single-conversation'),
+    url(r'^conversations/(?P<conversation_id>\d+)/edit/$', ConversationView.edit, name='edit-conversation'),
+    url(r'^conversations/(?P<conversation_id>\d+)/delete/$', ConversationView.delete, name='delete-conversation'),
 
     # Comments
     url(r'comments/create/$', CommentView.create, name='create-comment'),
