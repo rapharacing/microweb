818c080236c5945c2e29a32be98b77ccdf45ab34
Matt Cottingham <matt.cottingham@gmail.com>
2013-08-06 09:50:53 +0100

    Add basic timing middleware which reports to riemann

---
+++ microcosm/middleware/timing.py
@@ -0,0 +1,28 @@
+from django.core.exceptions import MiddlewareNotUsed
+
+import microweb
+import bernhard
+import time
+
+
+class TimingMiddleware():
+
+    def __init__(self):
+        if microweb.settings.RIEMANN_ENABLED:
+            self.client = bernhard.Client(host=microweb.settings.RIEMANN_HOST, transport=bernhard.UDPTransport)
+        else:
+            raise MiddlewareNotUsed
+
+    def process_request(self, request):
+        request.start_time = time.time()
+        return None
+
+    def process_response(self, request, response):
+        delta = time.time() - request.start_time
+        self.client.send({
+            'host': 'django',
+            'service': 'microweb',
+            'metric': delta,
+            'tags' : ['timing'],
+        })
+        return response
