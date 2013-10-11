c83b623f01c11e1f3d47c9e7128189304d690282
David Kitchen <david@buro9.com>
2013-10-11 14:44:13 +0100

    Updated dependencies to be fully stand-alone

--- dependencies.sh
+++ dependencies.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-sudo apt-get -y install libevent1-dev libmemcached-dev
+sudo apt-get -y install build-essential libevent1-dev libmemcached-dev python-pip
+sudo pip install virtualenv
 
 virtualenv ENV
 source ENV/bin/activate
+ORIGPATH=$PATH
+export PATH=$PWD/ENV/bin:$PATH
 pip install -r requirements.txt
 deactivate
+export PATH=$ORIGPATH
