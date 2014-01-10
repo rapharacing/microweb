fa12d2179f15840fea2ea54a9808d4411fb08b9c
Jim Le <jim@heightdigital.co.uk>
2014-01-10 14:14:20 +0000

    Added comments template tag ex usage: {% comments <comment(s) object> [**kwargs] %}
    BUGFIX #87: Numbering of replies in a conversation start from 1 on subsequent pages

---
+++ microcosm/templatetags/comments.py
@@ -0,0 +1,14 @@
+from django import template
+import collections
+register = template.Library()
+
+@register.inclusion_tag('block_comments.html',takes_context=True)
+def comments(context, comments, **kwargs):
+
+  if not isinstance(comments, collections.Iterable):
+    comments = [comments]
+
+  context['comments'] = comments
+  context['hide_permalink'] = True if 'hide_permalink' in kwargs else False
+
+  return context
