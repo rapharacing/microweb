c0f8402fd83cc0bb1614256a769c6b0e3f6db601
Jim Le <jim@heightdigital.co.uk>
2014-01-17 17:06:07 +0000

    Removed debug print

--- microcosm/templatetags/conversation.py
+++ microcosm/templatetags/conversation.py
@@ -4,7 +4,6 @@ register = template.Library()
 @register.inclusion_tag('block_conversation.html',takes_context=True)
 def conversation(context, item, **kwargs):
 
-  print dir(item.item)
   if hasattr(item,'item'):
     context['item'] = item
   else:
