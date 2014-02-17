a69a63ffe834d9bf491baa97a0ec4fb6fec6e382
David Kitchen <david@buro9.com>
2014-02-17 17:39:52 +0000

    Added skeleton of templatetags to the updates page

--- microcosm/templatetags/event.py
+++ microcosm/templatetags/event.py
@@ -5,12 +5,6 @@ register = template.Library()
 @register.inclusion_tag('block_event.html',takes_context=True)
 def event(context, item, **kwargs):
 
-  if item.item.rsvp_limit > 0:
-    attending = item.item.rsvp_limit - item.item.rsvp_spaces
-    rsvp_percentage = int( math.ceil( (attending/float(item.item.rsvp_limit))*100 ) )
-
-    context['rsvp_percentage'] = rsvp_percentage
-
   if hasattr(item,'item'):
     context['item'] = item
   else:
