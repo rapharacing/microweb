0c16e958e18bb426661f98f551e5e3eac1f93e16
Matt Cottingham <matt.cottingham@gmail.com>
2014-03-20 14:21:39 +0000

    Add monkey patching of gevent to manage.py
    In the new relic python agent log, the following exception was observed:
    
    "NotImplementedError: gevent is only usable from a single thread"
    
    The fix for this is to use gevent's monkey.patch() method when the app
    is launched, which replaces functions in the standard library with
    versions that can be run concurrently.

--- manage.py
+++ manage.py
@@ -3,8 +3,10 @@ import os
 import sys
 
 if __name__ == "__main__":
+    import gevent
+    from gevent import monkey
+    monkey.patch_all()
+    
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "microweb.settings")
-
     from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)
