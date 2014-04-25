df575a8462cb396d5b12abe835ac52007baa4108
Matt Cottingham <matt.cottingham@gmail.com>
2014-04-25 10:48:52 +0100

    Remove unused import and fix indentation in commentBox.py

--- microcosm/templatetags/commentBox.py
+++ microcosm/templatetags/commentBox.py
@@ -1,23 +1,21 @@
 from django import template
-import collections
 register = template.Library()
 
 @register.inclusion_tag('forms/block_comment_box.html',takes_context=True)
 def commentBox(context, **kwargs):
 
-  # as_component - removes <form> element and submit buttons in block_comment_box.html
-  context['as_component']   = True if 'as_component' in kwargs else False
+    # as_component - removes <form> element and submit buttons in block_comment_box.html
+    context['as_component']   = True if 'as_component' in kwargs else False
 
-  # no_attachments - removes the attachment section of the comment box
-  context['no_attachments'] = True if 'no_attachments' in kwargs else False
+    # no_attachments - removes the attachment section of the comment box
+    context['no_attachments'] = True if 'no_attachments' in kwargs else False
 
-  # name - used as name attribute in <textarea>
-  if 'name' in kwargs:
-    context['name'] = kwargs['name']
+    # name - used as name attribute in <textarea>
+    if 'name' in kwargs:
+        context['name'] = kwargs['name']
 
-  # value = used in <textarea>
+    # value = used in <textarea>
+    if 'value' in kwargs:
+        context['value'] = kwargs['value']
 
-  if 'value' in kwargs:
-    context['value'] = kwargs['value']
-
-  return context
+    return context
