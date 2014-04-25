4b5a73ee686e85869d0dad81f98dcd0934936753
Matt Cottingham <matt.cottingham@gmail.com>
2014-04-25 11:05:01 +0100

    PEP8 conversation.py

--- microcosm/templatetags/conversation.py
+++ microcosm/templatetags/conversation.py
@@ -4,14 +4,14 @@ register = template.Library()
 @register.inclusion_tag('block_conversation.html',takes_context=True)
 def conversation(context, item, **kwargs):
 
-	if hasattr(item,'item'):
-		context['item'] = item
-	else:
-		context['item'] = { 'item' : item }
+    if hasattr(item,'item'):
+        context['item'] = item
+    else:
+        context['item'] = { 'item' : item }
 
-	if 'unread' in kwargs:
-		context['unread'] = kwargs['unread']
-	else:
-		context['unread'] = False
+    if 'unread' in kwargs:
+        context['unread'] = kwargs['unread']
+    else:
+        context['unread'] = False
 
-	return context
+    return context
