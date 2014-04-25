2bbedd42439e01e5f0a54799a7fbc189b814507a
Matt Cottingham <matt.cottingham@gmail.com>
2014-04-25 10:54:21 +0100

    PEP8 comments.py

--- microcosm/templatetags/comments.py
+++ microcosm/templatetags/comments.py
@@ -2,13 +2,13 @@ from django import template
 import collections
 register = template.Library()
 
-@register.inclusion_tag('block_comments.html',takes_context=True)
+@register.inclusion_tag('block_comments.html', takes_context=True)
 def comments(context, comments, **kwargs):
 
-  if not isinstance(comments, collections.Iterable):
-    comments = [comments]
+    if not isinstance(comments, collections.Iterable):
+        comments = [comments]
 
-  context['comments'] = comments
-  context['hide_permalink'] = True if 'hide_permalink' in kwargs else False
+    context['comments'] = comments
+    context['hide_permalink'] = True if 'hide_permalink' in kwargs else False
 
-  return context
+    return context
