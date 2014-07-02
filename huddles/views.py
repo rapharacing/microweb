0fc0698cd453a3ef758cec4ebfe50d6c02cd0169
Matt Cottingham <matt.cottingham@gmail.com>
2014-07-02 17:12:23 +0100

    Handle API exceptions directly in huddle views

--- huddles/views.py
+++ huddles/views.py
@@ -19,12 +19,13 @@ from django.views.decorators.http import require_http_methods
 from core.api.resources import Comment
 from core.api.resources import Profile
 from core.api.resources import Attachment
-from core.api.resources import response_list_to_dict
 from core.api.resources import Site
 from core.api.resources import Huddle
 from core.api.resources import HuddleList
+from core.api.resources import APIException
+from core.api.resources import response_list_to_dict
 
-from core.views import exception_handler
+from core.views import ErrorView
 from core.views import require_authentication
 from core.views import build_pagination_links
 
@@ -42,7 +43,6 @@ class HuddleView(object):
     list_template = 'huddles.html'
 
     @staticmethod
-    @exception_handler
     @require_authentication
     @require_http_methods(['GET',])
     def single(request, huddle_id):
@@ -53,7 +53,16 @@ class HuddleView(object):
         huddle_url, params, headers = Huddle.build_request(request.get_host(), id=huddle_id, offset=offset,
             access_token=request.access_token)
         request.view_requests.append(grequests.get(huddle_url, params=params, headers=headers))
-        responses = response_list_to_dict(grequests.map(request.view_requests))
+        try:
+            responses = response_list_to_dict(grequests.map(request.view_requests))
+        except APIException as e:
+            if e.status_code == 404:
+                return ErrorView.not_found(request)
+            elif e.status_code == 403:
+                return ErrorView.forbidden(request)
+            else:
+                return ErrorView.server_error(request)
+
         huddle = Huddle.from_api_response(responses[huddle_url])
         comment_form = CommentForm(initial=dict(itemId=huddle_id, itemType='huddle'))
 
@@ -84,7 +93,6 @@ class HuddleView(object):
 
 
     @staticmethod
-    @exception_handler
     @require_http_methods(['GET',])
     def list(request):
         # record offset for paging of huddles
@@ -94,7 +102,15 @@ class HuddleView(object):
             access_token=request.access_token)
 
         request.view_requests.append(grequests.get(huddle_url, params=params, headers=headers))
-        responses = response_list_to_dict(grequests.map(request.view_requests))
+        try:
+            responses = response_list_to_dict(grequests.map(request.view_requests))
+        except APIException as e:
+            if e.status_code == 404:
+                return ErrorView.not_found(request)
+            elif e.status_code == 403:
+                return ErrorView.forbidden(request)
+            else:
+                return ErrorView.server_error(request)
 
         huddles = HuddleList(responses[huddle_url])
 
@@ -108,7 +124,6 @@ class HuddleView(object):
 
 
     @staticmethod
-    @exception_handler
     @require_authentication
     @require_http_methods(['GET', 'POST',])
     def create(request):
@@ -116,32 +131,47 @@ class HuddleView(object):
         Create a huddle.
         """
 
-        responses = response_list_to_dict(grequests.map(request.view_requests))
+        try:
+            responses = response_list_to_dict(grequests.map(request.view_requests))
+        except APIException as e:
+            if e.status_code == 404:
+                return ErrorView.not_found(request)
+            elif e.status_code == 403:
+                return ErrorView.forbidden(request)
+            else:
+                return ErrorView.server_error(request)
+
         view_data = {
             'user': Profile(responses[request.whoami_url], summary=False),
             'site': Site(responses[request.site_url]),
-            }
+        }
 
         if request.method == 'POST':
             form = HuddleView.create_form(request.POST)
             if form.is_valid():
                 hud_request = Huddle.from_create_form(form.cleaned_data)
-                hud_response = hud_request.create(request.get_host(), request.access_token)
-                if hud_response.id > 0:
-                    if request.POST.get('invite'):
-                        ids = [int(x) for x in request.POST.get('invite').split(',')]
-                        Huddle.invite(request.get_host(), hud_response.id, ids, request.access_token)
+                try:
+                    hud_response = hud_request.create(request.get_host(), request.access_token)
+                except APIException as e:
+                    if e.status_code == 403:
+                        return ErrorView.forbidden(request)
+                    else:
+                        return ErrorView.server_error(request)
 
-                    if request.POST.get('firstcomment') and len(request.POST.get('firstcomment')) > 0:
-                        payload = {
-                            'itemType': 'huddle',
-                            'itemId': hud_response.id,
-                            'markdown': request.POST.get('firstcomment'),
-                            'inReplyTo': 0
-                        }
-                        comment = Comment.from_create_form(payload)
-                        comment.create(request.get_host(), request.access_token)
-                    return HttpResponseRedirect(reverse('single-huddle', args=(hud_response.id,)))
+                if request.POST.get('invite'):
+                    ids = [int(x) for x in request.POST.get('invite').split(',')]
+                    Huddle.invite(request.get_host(), hud_response.id, ids, request.access_token)
+
+                if request.POST.get('firstcomment') and len(request.POST.get('firstcomment')) > 0:
+                    payload = {
+                        'itemType': 'huddle',
+                        'itemId': hud_response.id,
+                        'markdown': request.POST.get('firstcomment'),
+                        'inReplyTo': 0
+                    }
+                    comment = Comment.from_create_form(payload)
+                    comment.create(request.get_host(), request.access_token)
+                return HttpResponseRedirect(reverse('single-huddle', args=(hud_response.id,)))
             else:
                 view_data['form'] = form
                 return render(request, HuddleView.form_template, view_data)
@@ -152,22 +182,22 @@ class HuddleView(object):
                 list_of_recipient_ids = request.GET.get('to').split(",")
 
                 for recipient_id in list_of_recipient_ids:
-                    recipient_profile = Profile.retrieve(request.get_host(), recipient_id)
-                    if recipient_profile.id > 0:
-                        recipients.append({
-                            'id': recipient_profile.id,
-                            'profileName': recipient_profile.profile_name,
-                            'avatar': recipient_profile.avatar
-                        })
-
+                    try:
+                        recipient_profile = Profile.retrieve(request.get_host(), recipient_id)
+                    except APIException:
+                        # Skip this recipient, but don't return as we may be able to load the others.
+                        continue
+                    recipients.append({
+                        'id': recipient_profile.id,
+                        'profileName': recipient_profile.profile_name,
+                        'avatar': recipient_profile.avatar
+                    })
                 view_data['recipients_json'] = json.dumps(recipients)
 
             view_data['form'] = HuddleView.create_form(initial=dict())
             return render(request, HuddleView.form_template, view_data)
 
-
     @staticmethod
-    @exception_handler
     @require_authentication
     @require_http_methods(['POST', ])
     def invite(request, huddle_id):
@@ -176,12 +206,19 @@ class HuddleView(object):
         """
 
         ids = [int(x) for x in request.POST.get('invite_profile_id').split()]
-        Huddle.invite(request.get_host(), huddle_id, ids, request.access_token)
+        try:
+            Huddle.invite(request.get_host(), huddle_id, ids, request.access_token)
+        except APIException as e:
+            if e.status_code == 404:
+                return ErrorView.not_found(request)
+            elif e.status_code == 403:
+                return ErrorView.forbidden(request)
+            else:
+                return ErrorView.server_error(request)
+
         return HttpResponseRedirect(reverse('single-huddle', args=(huddle_id,)))
 
-
     @staticmethod
-    @exception_handler
     @require_authentication
     @require_http_methods(['POST', ])
     def delete(request, huddle_id):
@@ -189,13 +226,20 @@ class HuddleView(object):
         Delete a huddle and be redirected to the parent microcosm.
         """
 
-        huddle = Huddle.retrieve(request.get_host(), huddle_id, access_token=request.access_token)
-        huddle.delete(request.get_host(), request.access_token)
+        try:
+            huddle = Huddle.retrieve(request.get_host(), huddle_id, access_token=request.access_token)
+            huddle.delete(request.get_host(), request.access_token)
+        except APIException as e:
+            if e.status_code == 404:
+                return ErrorView.not_found(request)
+            elif e.status_code == 403:
+                return ErrorView.forbidden(request)
+            else:
+                return ErrorView.server_error(request)
+
         return HttpResponseRedirect(reverse('list-huddle'))
 
-
     @staticmethod
-    @exception_handler
     @require_authentication
     @require_http_methods(['GET', ])
     def newest(request, huddle_id):
@@ -203,7 +247,16 @@ class HuddleView(object):
         Get redirected to the first unread post in a huddle
         """
 
-        response = Huddle.newest(request.get_host(), huddle_id, access_token=request.access_token)
+        try:
+            response = Huddle.newest(request.get_host(), huddle_id, access_token=request.access_token)
+        except APIException as e:
+            if e.status_code == 404:
+                return ErrorView.not_found(request)
+            elif e.status_code == 403:
+                return ErrorView.forbidden(request)
+            else:
+                return ErrorView.server_error(request)
+
         #because redirects are always followed, we can't just get the 'location' value
         response = response['comments']['links']
         for link in response:
