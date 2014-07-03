3099c84592158be8316a6adec4abb6b5f8d0ef40
Matt Cottingham <matt.cottingham@gmail.com>
2014-07-03 12:03:00 +0100

    Use module-level objects in profiles/views

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 from django.conf.urls import patterns
 
-from profiles.views import ProfileView
+from profiles import views
 
 urlpatterns = patterns('',
-    url(r'^profiles/$', ProfileView.list, name='list-profiles'),
-    url(r'^profiles/(?P<profile_id>\d+)/$', ProfileView.single, name='single-profile'),
-    url(r'^profiles/(?P<profile_id>\d+)/edit/$', ProfileView.edit, name='edit-profile'),
-    url(r'^profiles/read/$', ProfileView.mark_read, name='mark-read'),
+    url(r'^profiles/$', views.list, name='list-profiles'),
+    url(r'^profiles/(?P<profile_id>\d+)/$', views.single, name='single-profile'),
+    url(r'^profiles/(?P<profile_id>\d+)/edit/$', views.edit, name='edit-profile'),
+    url(r'^profiles/read/$', views.mark_read, name='mark-read'),
 )
--- profiles/views.py
+++ profiles/views.py
@@ -26,192 +26,191 @@ from core.views import build_pagination_links
 from core.views import require_authentication
 from core.views import respond_with_error
 
+
 logger = logging.getLogger('profiles.views')
+edit_form = ProfileEdit
+form_template = 'forms/profile.html'
+single_template = 'profile.html'
+list_template = 'profiles.html'
 
 
-class ProfileView(object):
-    edit_form = ProfileEdit
-    form_template = 'forms/profile.html'
-    single_template = 'profile.html'
-    list_template = 'profiles.html'
+@require_http_methods(['GET', ])
+def single(request, profile_id):
+    """
+    Display a single profile by ID.
+    """
 
-    @staticmethod
-    @require_http_methods(['GET', ])
-    def single(request, profile_id):
-        """
-        Display a single profile by ID.
-        """
+    # Fetch items created by this profile.
+    search_q = 'type:microcosm type:conversation type:event type:comment authorId:%s' % profile_id
+    search_params = {'limit': 5, 'q': search_q, 'sort': 'date'}
+    search_url, params, headers = Search.build_request(request.get_host(), search_params,
+                                                       access_token=request.access_token)
+    request.view_requests.append(grequests.get(search_url, params=params, headers=headers))
 
-        # Fetch items created by this profile.
-        search_q = 'type:microcosm type:conversation type:event type:comment authorId:%s' % profile_id
-        search_params = {'limit': 5, 'q': search_q, 'sort': 'date'}
-        search_url, params, headers = Search.build_request(request.get_host(), search_params,
-                                                           access_token=request.access_token)
-        request.view_requests.append(grequests.get(search_url, params=params, headers=headers))
+    try:
+        responses = response_list_to_dict(grequests.map(request.view_requests))
+    except APIException as exc:
+        return respond_with_error(request, exc)
 
-        try:
-            responses = response_list_to_dict(grequests.map(request.view_requests))
-        except APIException as exc:
-            return respond_with_error(request, exc)
+    profile = Profile(responses[request.whoami_url], summary=False) if request.whoami_url else None,
 
-        profile = Profile(responses[request.whoami_url], summary=False) if request.whoami_url else None,
+    view_data = {
+        'user': profile,
+        'content': profile,
+        'item_type': 'profile',
+        'site': Site(responses[request.site_url]),
+        'search': Search.from_api_response(responses[search_url]),
+        'site_section': 'people'
+    }
 
-        view_data = {
-            'user': profile,
-            'content': profile,
-            'item_type': 'profile',
-            'site': Site(responses[request.site_url]),
-            'search': Search.from_api_response(responses[search_url]),
-            'site_section': 'people'
-        }
+    return render(request, single_template, view_data)
 
-        return render(request, ProfileView.single_template, view_data)
 
-    @staticmethod
-    @require_http_methods(['GET', ])
-    def list(request):
+@require_http_methods(['GET', ])
+def list(request):
 
-        # Record offset for paging of profiles.
-        offset = int(request.GET.get('offset', 0))
-        top = bool(request.GET.get('top', False))
-        q = request.GET.get('q', "")
-        following = bool(request.GET.get('following', False))
-        online = bool(request.GET.get('online', False))
+    # Record offset for paging of profiles.
+    offset = int(request.GET.get('offset', 0))
+    top = bool(request.GET.get('top', False))
+    q = request.GET.get('q', "")
+    following = bool(request.GET.get('following', False))
+    online = bool(request.GET.get('online', False))
 
-        profiles_url, params, headers = ProfileList.build_request(request.get_host(), offset=offset, top=top,
-            q=q, following=following, online=online, access_token=request.access_token)
+    profiles_url, params, headers = ProfileList.build_request(request.get_host(), offset=offset, top=top,
+        q=q, following=following, online=online, access_token=request.access_token)
 
-        request.view_requests.append(grequests.get(profiles_url, params=params, headers=headers))
-        try:
-            responses = response_list_to_dict(grequests.map(request.view_requests))
-        except APIException as exc:
-            return respond_with_error(request, exc)
+    request.view_requests.append(grequests.get(profiles_url, params=params, headers=headers))
+    try:
+        responses = response_list_to_dict(grequests.map(request.view_requests))
+    except APIException as exc:
+        return respond_with_error(request, exc)
 
-        profiles = ProfileList(responses[profiles_url])
+    profiles = ProfileList(responses[profiles_url])
 
-        subtitle = False
-        if q != "" and len(q) == 1:
-            subtitle = "names starting with %s" % (q.upper())
+    subtitle = False
+    if q != "" and len(q) == 1:
+        subtitle = "names starting with %s" % (q.upper())
 
-        filter_name = []
+    filter_name = []
 
-        if following:
-            filter_name.append("following")
+    if following:
+        filter_name.append("following")
 
-        if online:
-            filter_name.append("online now")
+    if online:
+        filter_name.append("online now")
 
-        if top:
-            filter_name.append("most comments")
+    if top:
+        filter_name.append("most comments")
 
-        if len(filter_name) < 1:
-            filter_name.append("sorted alphabetically")
+    if len(filter_name) < 1:
+        filter_name.append("sorted alphabetically")
 
-        view_data = {
-            'user': Profile(responses[request.whoami_url], summary=False) if request.whoami_url else None,
-            'site': Site(responses[request.site_url]),
-            'content': profiles,
-            'pagination': build_pagination_links(responses[profiles_url]['profiles']['links'], profiles.profiles),
-            'q': q,
-            'top': top,
-            'following': following,
-            'alphabet': string.ascii_lowercase,
-            'site_section': 'people',
-            'filter_name': ", ".join(filter_name),
-            'subtitle': subtitle,
-            'online': online
-        }
+    view_data = {
+        'user': Profile(responses[request.whoami_url], summary=False) if request.whoami_url else None,
+        'site': Site(responses[request.site_url]),
+        'content': profiles,
+        'pagination': build_pagination_links(responses[profiles_url]['profiles']['links'], profiles.profiles),
+        'q': q,
+        'top': top,
+        'following': following,
+        'alphabet': string.ascii_lowercase,
+        'site_section': 'people',
+        'filter_name': ", ".join(filter_name),
+        'subtitle': subtitle,
+        'online': online
+    }
 
-        return render(request, ProfileView.list_template, view_data)
+    return render(request, list_template, view_data)
 
-    @staticmethod
-    @require_http_methods(['GET', 'POST',])
-    def edit(request, profile_id):
-        """
-        Edit a user profile (profile name or avatar).
-        """
 
-        try:
-            responses = response_list_to_dict(grequests.map(request.view_requests))
-        except APIException as exc:
-            return respond_with_error(request, exc)
-        user = Profile(responses[request.whoami_url], summary=False)
-        view_data = {
-            'user': user,
-            'site': Site(responses[request.site_url]),
-        }
+@require_authentication
+@require_http_methods(['GET', 'POST',])
+def edit(request, profile_id):
+    """
+    Edit a user profile (profile name or avatar).
+    """
 
-        if request.method == 'POST':
-            form = ProfileView.edit_form(request.POST)
-            if form.is_valid():
-                # Upload new avatar if present.
-                if request.FILES.has_key('avatar'):
-                    file_request = FileMetadata.from_create_form(request.FILES['avatar'])
-                    file_metadata = file_request.create(request.get_host(), request.access_token, 100, 100)
+    try:
+        responses = response_list_to_dict(grequests.map(request.view_requests))
+    except APIException as exc:
+        return respond_with_error(request, exc)
+    user = Profile(responses[request.whoami_url], summary=False)
+    view_data = {
+        'user': user,
+        'site': Site(responses[request.site_url]),
+    }
+
+    if request.method == 'POST':
+        form = edit_form(request.POST)
+        if form.is_valid():
+            # Upload new avatar if present.
+            if request.FILES.has_key('avatar'):
+                file_request = FileMetadata.from_create_form(request.FILES['avatar'])
+                file_metadata = file_request.create(request.get_host(), request.access_token, 100, 100)
+                try:
+                    Attachment.create(request.get_host(), file_metadata.file_hash, profile_id=user.id,
+                        access_token=request.access_token, file_name=request.FILES['avatar'].name)
+                except APIException as exc:
+                    return respond_with_error(request, exc)
+
+            # Update the actual profile resource.
+            profile_request = Profile(form.cleaned_data)
+            profile_response = profile_request.update(request.get_host(), request.access_token)
+
+            # Update or create comment on profile (single comment acts as a bio) if submitted.
+            if request.POST.get('markdown'):
+                payload = {
+                    'itemType': 'profile',
+                    'itemId': profile_response.id,
+                    'markdown': request.POST.get('markdown'),
+                    'inReplyTo': 0
+                }
+
+                # If profile already has an attached comment update it, otherwise create a new one.
+                if hasattr(profile_response, 'profile_comment'):
+                    payload['id'] = profile_response.profile_comment.id
+                    if len(request.POST.get('markdown')) < 1:
+                        payload['markdown'] = ""
+                    comment_request = Comment.from_edit_form(payload)
                     try:
-                        Attachment.create(request.get_host(), file_metadata.file_hash, profile_id=user.id,
-                            access_token=request.access_token, file_name=request.FILES['avatar'].name)
+                        comment_request.update(request.get_host(), access_token=request.access_token)
                     except APIException as exc:
                         return respond_with_error(request, exc)
-
-                # Update the actual profile resource.
-                profile_request = Profile(form.cleaned_data)
-                profile_response = profile_request.update(request.get_host(), request.access_token)
-
-                # Update or create comment on profile (single comment acts as a bio) if submitted.
-                if request.POST.get('markdown'):
-                    payload = {
-                        'itemType': 'profile',
-                        'itemId': profile_response.id,
-                        'markdown': request.POST.get('markdown'),
-                        'inReplyTo': 0
-                    }
-
-                    # If profile already has an attached comment update it, otherwise create a new one.
-                    if hasattr(profile_response, 'profile_comment'):
-                        payload['id'] = profile_response.profile_comment.id
-                        if len(request.POST.get('markdown')) < 1:
-                            payload['markdown'] = ""
-                        comment_request = Comment.from_edit_form(payload)
+                else:
+                    if len(request.POST.get('markdown')) > 0:
+                        comment = Comment.from_create_form(payload)
                         try:
-                            comment_request.update(request.get_host(), access_token=request.access_token)
+                            comment.create(request.get_host(), request.access_token)
                         except APIException as exc:
                             return respond_with_error(request, exc)
-                    else:
-                        if len(request.POST.get('markdown')) > 0:
-                            comment = Comment.from_create_form(payload)
-                            try:
-                                comment.create(request.get_host(), request.access_token)
-                            except APIException as exc:
-                                return respond_with_error(request, exc)
 
-                return HttpResponseRedirect(reverse('single-profile', args=(profile_response.id,)))
-            else:
-                view_data['form'] = form
-                return render(request, ProfileView.form_template, view_data)
+            return HttpResponseRedirect(reverse('single-profile', args=(profile_response.id,)))
+        else:
+            view_data['form'] = form
+            return render(request, form_template, view_data)
 
-        if request.method == 'GET':
-            try:
-                user_profile = Profile.retrieve(request.get_host(), profile_id, request.access_token)
-            except APIException as exc:
-                return respond_with_error(request, exc)
-            view_data['form'] = ProfileView.edit_form(user_profile.as_dict)
-            return render(request, ProfileView.form_template, view_data)
-
-    @staticmethod
-    @require_authentication
-    @require_http_methods(['POST',])
-    def mark_read(request):
-        """
-        Mark a scope (e.g. site or microcosm) as read for the authenticated user.
-        """
-
-        scope = {
-            'itemType': request.POST.get('item_type'),
-            'itemId': int(request.POST.get('item_id')),
-        }
+    if request.method == 'GET':
         try:
-            Profile.mark_read(request.get_host(), scope, request.access_token)
+            user_profile = Profile.retrieve(request.get_host(), profile_id, request.access_token)
         except APIException as exc:
             return respond_with_error(request, exc)
-        return HttpResponseRedirect(request.POST.get('return_path'))
+        view_data['form'] = edit_form(user_profile.as_dict)
+        return render(request, form_template, view_data)
+
+
+@require_authentication
+@require_http_methods(['POST',])
+def mark_read(request):
+    """
+    Mark a scope (e.g. site or microcosm) as read for the authenticated user.
+    """
+
+    scope = {
+        'itemType': request.POST.get('item_type'),
+        'itemId': int(request.POST.get('item_id')),
+    }
+    try:
+        Profile.mark_read(request.get_host(), scope, request.access_token)
+    except APIException as exc:
+        return respond_with_error(request, exc)
+    return HttpResponseRedirect(request.POST.get('return_path'))
