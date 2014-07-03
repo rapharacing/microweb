c7eca2fb4bb8e293f7922b46fb3e6aef0ca85c45
Matt Cottingham <matt.cottingham@gmail.com>
2014-07-03 14:30:08 +0100

    Improve error handling and clean up updates/watchers views

--- updates/urls.py
+++ updates/urls.py
@@ -1,16 +1,14 @@
 from django.conf.urls import url
 from django.conf.urls import patterns
 
-from updates.views import UpdateView
-from updates.views import WatcherView
-from updates.views import UpdatePreferenceView
+from updates import views
 
 
 urlpatterns = patterns('',
     # Updates
-    url(r'^updates/$', UpdateView.list, name='list-updates'),
-    url(r'^updates/settings/$', UpdatePreferenceView.settings, name='updates-settings'),
+    url(r'^updates/$', views.list_updates, name='list-updates'),
+    url(r'^updates/settings/$', views.settings, name='updates-settings'),
 
     # Watchers
-    url(r'^watchers/$', WatcherView.single, name='single-watcher'),
+    # url(r'^watchers/$', WatcherView.list, name='single-watcher'),
 )
--- updates/views.py
+++ updates/views.py
@@ -1,11 +1,8 @@
-import requests
 import grequests
 import logging
 
 from django.core.urlresolvers import reverse
 
-from django.http import HttpResponseBadRequest
-from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 
 from django.shortcuts import render
@@ -13,190 +10,181 @@ from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 
 from core.api.resources import UpdateList
-from core.api.resources import Update
 from core.api.resources import UpdatePreference
-from core.api.resources import WatcherList
-from core.api.resources import Watcher
 from core.api.resources import Profile
 from core.api.resources import response_list_to_dict
 from core.api.resources import GlobalOptions
 from core.api.resources import Site
 
-from core.views import exception_handler
+from core.api.exceptions import APIException
+
+from core.views import respond_with_error
 from core.views import build_pagination_links
 from core.views import require_authentication
 
+
 logger = logging.getLogger('updates.views')
+list_template = 'updates.html'
+settings_template = 'forms/update_settings.html'
+watchers_template = 'watchers.html'
 
 
-class UpdateView(object):
-    list_template = 'updates.html'
+@require_http_methods(['GET',])
+def list_updates(request):
 
-    @staticmethod
-    @exception_handler
-    @require_http_methods(['GET',])
-    def list(request):
-        # TODO: need a user friendly error page for unregistered users
-        # TODO: remove 'site_section'
-        if not request.access_token:
+    if not request.access_token:
+        try:
             responses = response_list_to_dict(grequests.map(request.view_requests))
-            view_data = {
-                'user': False,
-                'site_section': 'updates',
-                'site': Site(responses[request.site_url]),
-                }
-        else:
-            # pagination offset
-            offset = int(request.GET.get('offset', 0))
+        except APIException as exc:
+            return respond_with_error(request, exc)
+        view_data = {
+            'user': False,
+            'site_section': 'updates',
+            'site': Site(responses[request.site_url]),
+        }
+    else:
+        # pagination offset
+        offset = int(request.GET.get('offset', 0))
 
-            url, params, headers = UpdateList.build_request(request.get_host(), offset=offset,
-                                                            access_token=request.access_token)
-            request.view_requests.append(grequests.get(url, params=params, headers=headers))
+        url, params, headers = UpdateList.build_request(request.get_host(), offset=offset,
+            access_token=request.access_token)
+        request.view_requests.append(grequests.get(url, params=params, headers=headers))
+        try:
             responses = response_list_to_dict(grequests.map(request.view_requests))
-            updates_list = UpdateList(responses[url])
+        except APIException as exc:
+            return respond_with_error(request, exc)
+        updates_list = UpdateList(responses[url])
 
-            view_data = {
-                'user': Profile(responses[request.whoami_url], summary=False),
-                'content': updates_list,
-                'pagination': build_pagination_links(responses[url]['updates']['links'], updates_list.updates),
-                'site_section': 'updates',
-                'site': Site(responses[request.site_url]),
-                }
+        view_data = {
+            'user': Profile(responses[request.whoami_url], summary=False),
+            'content': updates_list,
+            'pagination': build_pagination_links(responses[url]['updates']['links'], updates_list.updates),
+            'site_section': 'updates',
+            'site': Site(responses[request.site_url]),
+        }
 
-        return render(request, UpdateView.list_template, view_data)
-
-    @staticmethod
-    @exception_handler
-    @require_authentication
-    @require_http_methods(['POST',])
-    def mark_viewed(request, update_id):
-        """
-        Mark a update as viewed by setting a 'viewed' attribute.
-        """
-
-        Update.mark_viewed(request.get_host(), update_id, request.access_token)
-        return HttpResponseRedirect(reverse('list-updates'))
+    return render(request, list_template, view_data)
 
 
-class WatcherView(object):
-    list_template = 'watchers.html'
+@require_authentication
+@require_http_methods(['GET', 'POST',])
+def settings(request):
 
-    @staticmethod
-    @exception_handler
-    @require_authentication
-    @require_http_methods(['GET', 'POST',])
-    def list(request):
+    if request.method == 'POST':
 
-        if request.method == 'POST':
-            if 'watcher_id' in request.POST:
-                watchers = request.POST.getlist('watcher_id')
-                # TODO: get rid of casts
-                for w in watchers:
-                    if request.POST.get('delete_watcher_' + str(w)):
-                        Watcher.delete(request.get_host(), w, request.access_token)
-                    else:
-                        postdata = {
-                            'id': int(w),
-                            'sendEmail': bool(request.POST.get('send_email_' + str(w))),
-                            'receiveSMS': False,
-                            }
-                        Watcher.update(request.get_host(), int(w), postdata, request.access_token)
-            return HttpResponseRedirect(reverse('list-watchers'))
-
-        if request.method == 'GET':
-            # pagination offset
-            offset = int(request.GET.get('offset', 0))
-
-            url, params, headers = WatcherList.build_request(request.get_host(), offset=offset,
-                                                             access_token=request.access_token)
-            request.view_requests.append(grequests.get(url, params=params, headers=headers))
-            responses = response_list_to_dict(grequests.map(request.view_requests))
-            watchers_list = WatcherList(responses[url])
-
-            view_data = {
-                'user': Profile(responses[request.whoami_url], summary=False),
-                'site': Site(responses[request.site_url]),
-                'content': watchers_list,
-                'pagination': build_pagination_links(responses[url]['watchers']['links'], watchers_list.watchers)
-            }
-
-            return render(request, WatcherView.list_template, view_data)
-
-
-    @staticmethod
-    @exception_handler
-    @require_http_methods(['POST',])
-    def single(request):
+        # Update global settings for notifications.
         postdata = {
-            'updateTypeId': 1,
-            'itemType': request.POST.get('itemType'),
-            'itemId': int(request.POST.get('itemId')),
-            }
-        if request.POST.get('delete'):
-            Watcher.delete(request.get_host(), postdata, request.access_token)
-            return HttpResponse()
-        elif request.POST.get('patch'):
-            postdata = {
-                'itemType': request.REQUEST.get('itemType'),
-                'itemId': int(request.REQUEST.get('itemId')),
-                'sendEmail': "true" == request.REQUEST.get('emailMe')
-            }
-            response = Watcher.update(request.get_host(), postdata, request.access_token)
-            if response.status_code == requests.codes.ok:
-                return HttpResponse()
-            else:
-                return HttpResponseBadRequest()
-        else:
-            responsedata = Watcher.create(request.get_host(), postdata, request.access_token)
-            return HttpResponse(responsedata, content_type='application/json')
-
-
-class UpdatePreferenceView(object):
-    list_template = 'forms/update_settings.html'
-
-    @staticmethod
-    @exception_handler
-    @require_authentication
-    @require_http_methods(['GET', 'POST',])
-    def settings(request):
-
-        if request.method == 'POST':
-            for x in range(1, 10):
-                if request.POST.get('id_' + str(x)):
-                    postdata = {
-                        'id': int(request.POST['id_' + str(x)]),
-                        'sendEmail': bool(request.POST.get('send_email_' + str(x))),
-                        'sendSMS': False,
-                        }
-                    UpdatePreference.update(
-                        request.get_host(),
-                        request.POST['id_' + str(x)],
-                        postdata,
-                        request.access_token
-                    )
-
-            postdata = {
-                'sendEmail': bool(request.POST.get('profile_receive_email')),
-                'sendSMS': False,
-                }
+            'sendEmail': bool(request.POST.get('profile_receive_email')),
+            'sendSMS': False,
+        }
+        try:
             GlobalOptions.update(request.get_host(), postdata, request.access_token)
-            return HttpResponseRedirect(reverse('updates-settings'))
+        except APIException as exc:
+            return respond_with_error(request, exc)
 
-        if request.method == 'GET':
-            url, params, headers = UpdatePreference.build_request(request.get_host(), request.access_token)
-            request.view_requests.append(grequests.get(url, params=params, headers=headers))
+        # Update settings for each notification type.
+        for x in range(1, 10):
+            if request.POST.get('id_' + str(x)):
+                postdata = {
+                    'id': int(request.POST['id_' + str(x)]),
+                    'sendEmail': bool(request.POST.get('send_email_' + str(x))),
+                    'sendSMS': False,
+                    }
+                try:
+                    UpdatePreference.update(request.get_host(), request.POST['id_' + str(x)],
+                        postdata, request.access_token)
+                except APIException as exc:
+                    return respond_with_error(request, exc)
 
-            url2, params2, headers2 = GlobalOptions.build_request(request.get_host(), request.access_token)
-            request.view_requests.append(grequests.get(url2, params=params2, headers=headers2))
+        return HttpResponseRedirect(reverse('updates-settings'))
 
+    if request.method == 'GET':
+        url, params, headers = UpdatePreference.build_request(request.get_host(), request.access_token)
+        request.view_requests.append(grequests.get(url, params=params, headers=headers))
+
+        url2, params2, headers2 = GlobalOptions.build_request(request.get_host(), request.access_token)
+        request.view_requests.append(grequests.get(url2, params=params2, headers=headers2))
+
+        try:
             responses = response_list_to_dict(grequests.map(request.view_requests))
-            preference_list = UpdatePreference.from_list(responses[url])
-            global_options = GlobalOptions.from_api_response(responses[url2])
+        except APIException as exc:
+            return respond_with_error(request, exc)
 
-            view_data = {
-                'user': Profile(responses[request.whoami_url], summary=False),
-                'site': Site(responses[request.site_url]),
-                'content': preference_list,
-                'globaloptions': global_options,
-                }
-            return render(request, UpdatePreferenceView.list_template, view_data)
+        preference_list = UpdatePreference.from_list(responses[url])
+        global_options = GlobalOptions.from_api_response(responses[url2])
+
+        view_data = {
+            'user': Profile(responses[request.whoami_url], summary=False),
+            'site': Site(responses[request.site_url]),
+            'content': preference_list,
+            'globaloptions': global_options,
+        }
+        return render(request, settings_template, view_data)
+
+
+"""
+@require_authentication
+@require_http_methods(['GET', 'POST',])
+def list_watchers(request):
+
+    if request.method == 'POST':
+        if 'watcher_id' in request.POST:
+            watchers = request.POST.getlist('watcher_id')
+            for w in watchers:
+                if request.POST.get('delete_watcher_' + str(w)):
+                    Watcher.delete(request.get_host(), w, request.access_token)
+                else:
+                    postdata = {
+                        'id': int(w),
+                        'sendEmail': bool(request.POST.get('send_email_' + str(w))),
+                        'receiveSMS': False,
+                        }
+                    Watcher.update(request.get_host(), int(w), postdata, request.access_token)
+        return HttpResponseRedirect(reverse('list-watchers'))
+
+    if request.method == 'GET':
+        # pagination offset
+        offset = int(request.GET.get('offset', 0))
+
+        url, params, headers = WatcherList.build_request(request.get_host(), offset=offset,
+                                                         access_token=request.access_token)
+        request.view_requests.append(grequests.get(url, params=params, headers=headers))
+        responses = response_list_to_dict(grequests.map(request.view_requests))
+        watchers_list = WatcherList(responses[url])
+
+        view_data = {
+            'user': Profile(responses[request.whoami_url], summary=False),
+            'site': Site(responses[request.site_url]),
+            'content': watchers_list,
+            'pagination': build_pagination_links(responses[url]['watchers']['links'], watchers_list.watchers)
+        }
+
+        return render(request, watchers_template, view_data)
+
+
+@require_authentication
+@require_http_methods(['POST',])
+def delete_watcher(request):
+    postdata = {
+        'updateTypeId': 1,
+        'itemType': request.POST.get('itemType'),
+        'itemId': int(request.POST.get('itemId')),
+        }
+    if request.POST.get('delete'):
+        Watcher.delete(request.get_host(), postdata, request.access_token)
+        return HttpResponse()
+    elif request.POST.get('patch'):
+        postdata = {
+            'itemType': request.REQUEST.get('itemType'),
+            'itemId': int(request.REQUEST.get('itemId')),
+            'sendEmail': "true" == request.REQUEST.get('emailMe')
+        }
+        response = Watcher.update(request.get_host(), postdata, request.access_token)
+        if response.status_code == requests.codes.ok:
+            return HttpResponse()
+        else:
+            return HttpResponseBadRequest()
+    else:
+        responsedata = Watcher.create(request.get_host(), postdata, request.access_token)
+        return HttpResponse(responsedata, content_type='application/json')
+"""
