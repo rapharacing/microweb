dac26986069f56ab665e23f06533da777a2f71db
Matt Cottingham <matt.cottingham@gmail.com>
2014-07-03 11:18:23 +0100

    Update events.urls to use event view functions

--- events/urls.py
+++ events/urls.py
@@ -1,19 +1,18 @@
 from django.conf.urls import url
 from django.conf.urls import patterns
 
-from events.views import EventView
-from events.views import GeoView
+from events import views
 
 urlpatterns = patterns('',
     # Events
-    url(r'^microcosms/(?P<microcosm_id>\d+)/create/event/$', EventView.create, name='create-event'),
-    url(r'^events/(?P<event_id>\d+)/$', EventView.single, name='single-event'),
-    url(r'^events/(?P<event_id>\d+)/edit/$', EventView.edit, name='edit-event'),
-    url(r'^events/(?P<event_id>\d+)/delete/$', EventView.delete, name='delete-event'),
-    url(r'^events/(?P<event_id>\d+)/newest/$', EventView.newest, name='newest-event'),
+    url(r'^microcosms/(?P<microcosm_id>\d+)/create/event/$', views.create, name='create-event'),
+    url(r'^events/(?P<event_id>\d+)/$', views.single, name='single-event'),
+    url(r'^events/(?P<event_id>\d+)/edit/$', views.edit, name='edit-event'),
+    url(r'^events/(?P<event_id>\d+)/delete/$', views.delete, name='delete-event'),
+    url(r'^events/(?P<event_id>\d+)/newest/$', views.newest, name='newest-event'),
     # RSVP to an event
-    url(r'^events/(?P<event_id>\d+)/rsvp/$', EventView.rsvp, name='rsvp-event'),
+    url(r'^events/(?P<event_id>\d+)/rsvp/$', views.rsvp, name='rsvp-event'),
 
     # Proxy geocoding requests to the backend
-    url(r'^geocode/$', GeoView.geocode, name='geocode'),
+    url(r'^geocode/$', views.geocode, name='geocode'),
 )
