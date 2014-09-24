b84a19edda8003bf04a879c18a72a7ffb7ceef7b
David Kitchen <david@buro9.com>
2014-09-24 14:12:58 +0100

    Fixed incorrect ordering of URLs by restoring the redirects url to the bottom

--- microweb/urls.py
+++ microweb/urls.py
@@ -5,23 +5,24 @@ from core.views import ErrorView
 
 from microweb import settings
 
-
+# In the following, redirect.urls *MUST* remain the very last URL as it handles
+# all urls as potentially being a 404
 urlpatterns = patterns(
     '',
     url(r'', include('microcosms.urls')),
-    url(r'', include('comments.urls')),
-    url(r'', include('conversations.urls')),
     url(r'', include('core.urls')),
+    url(r'', include('conversations.urls')),
     url(r'', include('events.urls')),
     url(r'', include('huddles.urls')),
-    url(r'', include('ignored.urls')),
-    url(r'', include('moderation.urls')),
+    url(r'', include('comments.urls')),
     url(r'', include('profiles.urls')),
-    url(r'', include('redirect.urls')),
+    url(r'', include('updates.urls')),
     url(r'', include('search.urls')),
     url(r'', include('today.urls')),
     url(r'', include('trending.urls')),
-    url(r'', include('updates.urls')),
+    url(r'', include('moderation.urls')),
+    url(r'', include('ignored.urls')),
+    url(r'', include('redirect.urls')),
 )
 
 # Serve static files with gunicorn if DEBUG is true.
