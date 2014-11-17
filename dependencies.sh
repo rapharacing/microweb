9d74a60a58557d16126ace6d415b6ceb0f2bd8bc
David Kitchen <david@buro9.com>
2014-11-17 14:50:33 +0000

    updated to work on osx as well as linux

--- dependencies.sh
+++ dependencies.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-sudo apt-get -y install build-essential fabric libevent1-dev libmemcached-dev python-pip python2.7-dev
-sudo pip install virtualenv
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+	sudo apt-get -y install build-essential fabric libevent1-dev libmemcached-dev python-pip python2.7-dev
+	sudo pip install virtualenv
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	brew install libmemcached
+	brew install python
+	pip install fabric
+	pip install virtualenv
+else
+	echo -e "${COL_RED}This script only works on Linux and OSX $COL_RESET"
+	exit 1
+fi
 
 virtualenv ENV
 source ENV/bin/activate
