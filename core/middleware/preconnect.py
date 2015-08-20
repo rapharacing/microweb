378f52d18b902e21c5a70c3bc5bb2c049270d246
David Kitchen <dkitchen@cloudflare.com>
2015-08-20 15:42:36 +0100

    Add persona static content to preconnect

--- core/middleware/preconnect.py
+++ core/middleware/preconnect.py
@@ -2,6 +2,6 @@ class PreconnectMiddleware():
     def process_response(self, request, response):
         
         if response['Content-Type'] == 'text/html; charset=utf-8':
-            response['Link'] = '<//cdnjs.cloudflare.com>; rel=preconnect, <//www.google-analytics.com>; rel=preconnect, <https://login.persona.org>; rel=preconnect, <https://fonts.gstatic.com>; rel=preconnect; crossorigin'
+            response['Link'] = '<//cdnjs.cloudflare.com>; rel=preconnect, <//www.google-analytics.com>; rel=preconnect, <https://login.persona.org>; rel=preconnect, <https://static.login.persona.org>; rel=preconnect, <https://fonts.gstatic.com>; rel=preconnect; crossorigin'
 
         return response
